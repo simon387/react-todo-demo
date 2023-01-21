e40348e97c495ae915b4a069c295c4161fdb7345
simone.celia <simone.celia@eng.it>
2023-01-21 21:44:06 +0100

    ke

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -6,6 +6,7 @@ function App() {
 		{rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
 		{rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
 		{rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'User One'},
+		{rowNumber: 4, rowDescription: 'Charge phone battery', rowAssigned: 'User One'},
 	];
 	return (
 		<div className='mt-5 container'>
