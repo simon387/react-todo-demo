e34fd428f0cc8d9ee6f398f4c39d04b0e7bcee48
simone.celia <simone.celia@eng.it>
2023-01-22 18:48:03 +0100

    l26

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -4,6 +4,7 @@ import TodoTable from "./components/TodoTable";
 import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
+	const [showAddTodoForm, setShowAddTodoForm] = useState(false);
 	const [todos, setTodos] = useState([
 		{rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
 		{rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
@@ -32,7 +33,8 @@ function App() {
 				</div>
 				<div className='card-body'>
 					<TodoTable todos={todos} deleteTodo={deleteTodo}/>
-					<NewTodoForm addTodo={addTodo}/>
+					<button className='btn btn-primary' onClick={() => setShowAddTodoForm(!showAddTodoForm)}>{showAddTodoForm ? 'Close New Todo' : 'New Todo'}</button>
+					{showAddTodoForm && <NewTodoForm addTodo={addTodo}/>}
 				</div>
 			</div>
 		</div>
