eb4d6684bc2200192595045c33e0ace543fc55fe
Simone Celia <simone.celia@simonecelia.it>
2023-01-22 20:31:56 +0100

    newles

---
+++ typescript-training/02-variables/sample-types.js
@@ -0,0 +1,10 @@
+var found = true;
+var grade = 88.3;
+var firstName = "Anup";
+var lastName = 'Kumar';
+// found = 0;
+console.log(found);
+console.log("The grade is: " + grade);
+console.log("Hi " + firstName + " " + lastName);
+// use template string
+console.log("Hi ".concat(firstName, " ").concat(lastName));
