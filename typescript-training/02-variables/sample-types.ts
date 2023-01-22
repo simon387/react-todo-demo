c231fb27e604a2b7ae492ca201078b443ce4168d
simone.celia <simone.celia@eng.it>
2023-01-22 20:31:56 +0100

    newles

---
+++ typescript-training/02-variables/sample-types.ts
@@ -0,0 +1,13 @@
+let found: boolean = true;
+let grade: number = 88.3;
+let firstName: string = "Anup";
+let lastName: string = 'Kumar';
+
+// found = 0;
+
+console.log(found);
+console.log("The grade is: "+ grade);
+console.log("Hi " + firstName + " " + lastName);
+
+// use template string
+console.log(`Hi ${firstName} ${lastName}`);
