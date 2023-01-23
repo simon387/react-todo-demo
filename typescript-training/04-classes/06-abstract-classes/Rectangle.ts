3254efdfea25b14cd46c87eff01f28e9e4a348c6
simone.celia <simone.celia@eng.it>
2023-01-23 21:24:57 +0100

    abs_clas

---
+++ typescript-training/04-classes/06-abstract-classes/Rectangle.ts
@@ -0,0 +1,31 @@
+import {Shape} from "./Shape";
+
+export class Rectangle extends Shape{
+	constructor(_x: number, _y: number, private _width: number, private _length: number) {
+		super(_x, _y);
+	}
+
+	get width(): number {
+		return this._width;
+	}
+
+	set width(value: number) {
+		this._width = value;
+	}
+
+	get length(): number {
+		return this._length;
+	}
+
+	set length(value: number) {
+		this._length = value;
+	}
+
+	public getInfo(): string {
+		return super.getInfo() + `, width=${this._width}, length=${this._length}`;
+	}
+
+	public calculateArea(): number {
+		return this._width * this._length;
+	}
+}
