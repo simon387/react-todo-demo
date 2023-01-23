737506328e2e61a74e36038f919a5bf74533d00a
Simone Celia <simone.celia@simonecelia.it>
2023-01-23 22:14:24 +0100

    fine_switch

--- react-todos/src/components/NewTodoForm.tsx
+++ react-todos/src/components/NewTodoForm.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-function NewTodoForm(props: {addTodo: Function}) {
+export const NewTodoForm: React.FC<{ addTodo: Function }> = (props) => {
 	const [description, setDescription] = useState('');
 	const [assigned, setAssigned] = useState('');
 	const submitTodo = () => {
@@ -15,16 +15,14 @@ function NewTodoForm(props: {addTodo: Function}) {
 			<form>
 				<div className='mb-3'>
 					<label className='form-label'>Assigned</label>
-					<input type='text' className='form-control' required onChange={e => setAssigned(e.target.value)} value={assigned} />
+					<input type='text' className='form-control' required onChange={e => setAssigned(e.target.value)} value={assigned}/>
 				</div>
 				<div className='mb-3'>
 					<label className='form-label'>Description</label>
-					<textarea className='form-control' rows={3} required onChange={e => setDescription(e.target.value)} value={description} />
+					<textarea className='form-control' rows={3} required onChange={e => setDescription(e.target.value)} value={description}/>
 				</div>
 				<button type='button' className='btn btn-primary mt-3' onClick={submitTodo}>Add Todo</button>
 			</form>
 		</div>
 	);
 }
-
-export default NewTodoForm;
